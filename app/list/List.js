bf447730d85ee74c9e5d2f67c4676bd9390ab2f1
Dustin DeHaven <dustin.dehaven@gmail.com>
2020-02-01 20:18:26 +0900

    basic css in place

--- app/list/List.js
+++ app/list/List.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Product from './Product';
 
 
@@ -6,17 +6,10 @@ const List = () => {
   
   const [products, setProducts] = useState(window.Seed.products);
   
-  
-  // fetch items and categories.  Only runs once on page load
-  // useEffect(()=>{
-
-  //   setProducts(window.Seed.products)
-
-  // }, [])
-  
   return (
     <div className="list_page">
-      <h1>Popular Products</h1>
+      <h1 className='header'>Popular Products</h1>
+      <hr></hr>
       <ul>{ products
               .map( product => <Product key={product.id} product={product} />)} 
       </ul>
--- app/list/Product.js
+++ app/list/Product.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
+
 const Product = (props) => {
   return (
-    <li>
-      <img loading="lazy" className="image" width="400px" height="400px" src={props.product.productImageUrl}/>
-      <img loading="lazy" className="image" width="20px"  height="20px"  src='images/ui/arrow.png'/>
-      <p>{props.product.votes}</p>
-      <a href={props.product.url}>{props.product.title}</a>
-      <p>{props.product.description}</p>
-      <p>Submitted by</p>
-      <img loading="lazy" className="image" width="20px"  height="20px"  src={props.product.submitterAvatarUrl}/>
+    <li className="product">
+      <img  className="image" src={props.product.productImageUrl}/>
+      <img  className="arrow" src='images/ui/arrow.png'/>
+      <p    className="votes">{props.product.votes}</p>
+      <a    className="title" href={props.product.url}>{props.product.title}</a>
+      <p    className="description">{props.product.description}</p>
+      <p    className="submitted">Submitted by</p>
+      <img  className="submitter" src={props.product.submitterAvatarUrl}/>
     </li>
   )
 }
